78669b3efd066f7e3cf9865e8484782bb1799950
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2021-01-11 11:42:31 +0100

    Force UTF-8 to correctly parse Unicode chars
    Chars like ěščřžýáíé were being incorrectly parsed. This fixes it.

--- parse-slack-export.ps1
+++ parse-slack-export.ps1
@@ -9,8 +9,8 @@ $rebuildhash = $false #rebuild the dm hash even if it's not empty
 Clear-Host
 $directories = $null
 
-$dms = Get-Content ($exportPath + "dms.json") | ConvertFrom-Json
-$Global:users = Get-Content ($exportPath + "users.json") | ConvertFrom-Json
+$dms = Get-Content -Encoding "UTF8" ($exportPath + "dms.json") | ConvertFrom-Json
+$Global:users = Get-Content -Encoding "UTF8" ($exportPath + "users.json") | ConvertFrom-Json
 #region Unused API stuff
 <#
 Was using the APIs for info before I noticed the files in the root export folder.
@@ -96,7 +96,7 @@ foreach($directory in $directories) {
         New-Item -Force $htmlFile
         $jsonFiles = Get-ChildItem $directory.FullName -Exclude *.html
         foreach($jsonFile in $jsonFiles) {
-            $jsonObjs = Get-Content $jsonFile.FullName | ConvertFrom-Json
+            $jsonObjs = Get-Content -Encoding "UTF8" $jsonFile.FullName | ConvertFrom-Json
             foreach($jsonObj in $jsonObjs) {
                 $message = $null
                 $timestamp = $null
